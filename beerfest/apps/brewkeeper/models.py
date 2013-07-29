b247d525542d7dddae69f4820b38c63ce91cf8c8
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2013-07-29 00:52:04 -0700

    fixes model errors

--- beerfest/apps/brewkeeper/models.py
+++ beerfest/apps/brewkeeper/models.py
@@ -4,7 +4,7 @@ from autoslug import AutoSlugField
 
 STATE_CHOICES = [(u'AK', u'AK'), (u'AL', u'AL'), (u'AR', u'AR'), (u'AZ', u'AZ'), (u'CA', u'CA'), (u'CO', u'CO'), (u'CT', u'CT'), ("DC", "Washington, D.C."), (u'DE', u'DE'), (u'FL', u'FL'), (u'GA', u'GA'), (u'HI', u'HI'), (u'IA', u'IA'), (u'ID', u'ID'), (u'IL', u'IL'), (u'IN', u'IN'), (u'KS', u'KS'), (u'KY', u'KY'), (u'LA', u'LA'), (u'MA', u'MA'), (u'MD', u'MD'), (u'ME', u'ME'), (u'MI', u'MI'), (u'MN', u'MN'), (u'MO', u'MO'), (u'MS', u'MS'), (u'MT', u'MT'), (u'NC', u'NC'), (u'ND', u'ND'), (u'NE', u'NE'), (u'NH', u'NH'), (u'NJ', u'NJ'), (u'NM', u'NM'), (u'NV', u'NV'), (u'NY', u'NY'), (u'OH', u'OH'), (u'OK', u'OK'), (u'OR', u'OR'), (u'PA', u'PA'), (u'RIL', u'RIL'), (u'SC', u'SC'), (u'SD', u'SD'), (u'TN', u'TN'), (u'TX', u'TX'), (u'UT', u'UT'), (u'VA', u'VA'), (u'VT', u'VT'), (u'WA', u'WA'), (u'WI', u'WI'), (u'WV', u'WV'), (u'WY', u'WY')]
 REGION_CHOICES = [('West Coast', 'West Coast'), ('East Coast', 'East Coast'), ('Central', 'Central'), ]
-BEER_RATING_CHOICES = [('A+', 'A+'), ('A', 'A'), ('A-', 'A'), ('B+', 'B+'), ('B', 'B'), ('B-', 'B-'), ('C+', 'C+'), ('C', 'C'), ('C-', 'C-'), ('D+', 'D+'), ('D', 'D'), ('D-, D-'), ('F', 'F')]
+BEER_RATING_CHOICES = [('A+', 'A+'), ('A', 'A'), ('A-', 'A'), ('B+', 'B+'), ('B', 'B'), ('B-', 'B-'), ('C+', 'C+'), ('C', 'C'), ('C-', 'C-'), ('D+', 'D+'), ('D', 'D'), ('D-', 'D-'), ('F', 'F'), ]
 
 
 class Brewery(models.Model):
@@ -53,7 +53,7 @@ class Beer(models.Model):
     plato = models.FloatField(blank=True, null=True, help_text='Strength')
     degrees_lovibond = models.FloatField(blank=True, null=True, help_text='Color Scale')
     color = models.CharField(max_length=30, blank=True, null=True)
-    rating = models.CharField(blank=True, null=True, help_text='F to A+', choices=BEER_RATING_CHOICES)
+    rating = models.CharField(max_length=2, blank=True, null=True, help_text='F to A+', choices=BEER_RATING_CHOICES)
 
     # beer_advocate ratings: look, smell, taste, feel, overall
 
